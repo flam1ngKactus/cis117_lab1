4fc72a2852addc7ba6e87c7614ec49cf0d11fa19
flam1ngKactus <hughes.erika@gmail.com>
2025-09-25 12:31:25 -0700

    Update Lab1_Group14_Brown_Hughes.py

--- Lab1_Group14_Brown_Hughes.py
+++ Lab1_Group14_Brown_Hughes.py
@@ -1,9 +1,10 @@
 from rock_paper_scissors import play_rock_paper_scissors as rps
+from guessing_game import guessing_game as gg
 
 if __name__ == "__main__":
     print("Hi Thaddeus!  Welcome to our GitHub Repository for Partner Lab 1 Collaboration")
     while True:
-        user_input = input("Which game would you like to play? Number Guessing (1) | Rock Paper Scissors (2) | NOTHING! I'M TIRED OF YOUR GAMES (0): ")
+        user_input = input("Which game would you like to play? Number Guessing Game (1) | Rock Paper Scissors (2) | NOTHING! I'M TIRED OF YOUR GAMES (0): ")
         
         if user_input.isdigit() != True:
             print("OPERATOR ERROR: Input must be a number!")
@@ -15,7 +16,7 @@ if __name__ == "__main__":
             print("Okay, have a nice day!")
             break
         elif user_input == 1:
-            rps() # place holder for guessing game
+            gg() # place holder for guessing game
         elif user_input == 2:
             rps()
         else:
